c6f78355e7a6268c66baf36c0becbb96fbffe3ce
daviestuda10-cpu <daviestuda10@gmail.com>
2026-06-06 11:24:12 -0300

    Move addToCart into cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,5 @@
 import { cart } from "../data/cart.js";
+import { products, addToCart } from "../data/products.js";
 
 let productsHTML = '';
 
@@ -58,34 +59,23 @@ const productGridHTML = document.querySelector('.js-products-grid')
 
 productGridHTML.innerHTML = productsHTML
 
+function updateCartQuantity(){
+  let cartQuantity = 0;
+
+  cart.forEach((cartItem) => {
+      cartQuantity += cartItem.quantity
+  })
+
+  document.querySelector('.js-cart-quatity').innerHTML = cartQuantity
+}
+
 document.querySelectorAll('.js-add-to-cart')
     .forEach((button) => {
         button.addEventListener('click', () =>{
             const productId = button.dataset.productId
-            
-            let matchingItem;
 
-            cart.forEach((item) => {
-                if(productId === item.productId){
-                    matchingItem = item 
-                }
-            })
+            addToCart(productId)
+            updateCartQuantity()
 
-            if (matchingItem){
-                matchingItem.quantity += 1;
-            }else{
-                cart.push({
-                    productId: productId,
-                    quantity: 1
-                })
-            }
-
-            let cartQuantity = 0;
-
-            cart.forEach((item) => {
-                cartQuantity += item.quantity
-            })
-
-            document.querySelector('.js-cart-quatity').innerHTML = cartQuantity
         });
     }); 
